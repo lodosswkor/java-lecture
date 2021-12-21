69a402664e414d380113ec6dba290419ede20bb7
뽀로리 <lodossw@gmail.com>
2021-12-21 19:24:43 +0900

    나의 첫 커밋

---
+++ Day02/src/HelloWorld.java
@@ -0,0 +1,27 @@
+// 주석 설명
+
+/**
+ * JavaDoc 주석 
+ * 
+ * @author TJ  
+ *
+ */
+
+/* 여러줄 주석 
+ 작성자 : 이종석
+ 작성일 : 2021.12.17 
+ 내용 : HelloWorld 출력 
+*/
+//클래스 정의부 (한줄 주석) 
+
+public class HelloWorld {
+	
+	//어플리케이션 스타팅 포인트 (메인메소드)
+	public static void main(String args[]) {
+		
+		//Hello world 출력
+		System.out.println("Hello World"); 
+	
+	}
+	
+}
