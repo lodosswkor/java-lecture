5885a44d0e4242498be90e729b2263d89a2c8c1f
뽀로리 <lodossw@gmail.com>
2021-12-23 20:02:12 +0900

    할당 - 증감(단항)연산까지

---
+++ Day04/src/AssignExample.java
@@ -0,0 +1,26 @@
+
+public class AssignExample {
+
+	public static void main(String[] args) {
+		
+		//-- 할당연산은 우에서 좌로 
+		//-- 좌측은 변수/상수 , 우측은 데이터(값) 
+		//-- '=' 할당연산자 라고 합니다.
+		int age = 44; // 변수를 선언과 동시에 44라는 값을 '할당'  
+		int age2 = 10; 
+		int age3 = age + age2 + 10 * 20;
+		
+		System.out.println(" age : " + age );
+		age = age + 10; 
+		System.out.println(" age : " + age ); 
+		
+		//--- 변수를 선언 후 할당 
+		int age4; // 변수를 선언하고. 
+		//age4 = age * 10 / 20; 
+		//System.out.println(age4); 
+		
+		
+	}
+
+}
+
---
+++ Day04/src/OpExample.java
@@ -0,0 +1,31 @@
+
+// 단항연산자(증감연산) 
+public class OpExample {
+	
+	public static void main(String[] args) {
+	    
+		// ++ 
+		int b = 10;
+		System.out.println(b++);
+		System.out.println(b);
+		
+		int c = 10; 
+		System.out.println(++c);
+		System.out.println(c);
+		
+		// -- d-- => d = d - 1;
+		int d = 10; 
+		System.out.println(d --);
+		System.out.println(d);
+		
+		int e = 10; 
+		System.out.println(-- e);
+		System.out.println(e);
+		
+		
+		
+		
+		
+	}
+	
+}
